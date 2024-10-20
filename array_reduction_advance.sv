66f78fe54ecc9e2979950675fe7b455f52b1d6b7
priyadarshi-shubh <148803073+priyadarshi-shubh@users.noreply.github.com>
2024-10-19 19:25:11 -0700

    Create array_reduction_advance.sv

---
+++ array_reduction_advance.sv
@@ -0,0 +1,5 @@
+//You are given a dynamic array of integers where each element can have a value between 0 and 100. You are required to write a function in SystemVerilog that:
+
+//Randomizes the dynamic array (with values between 0 and 100).
+//Computes the sum of all the even numbers in the array using array reduction methods.
+//Additionally, compute the sum of all the elements at odd indices.
